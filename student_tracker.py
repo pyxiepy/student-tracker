61a1c2d6a42c78c964a4b78d6ddf2561b239bef9
pixipy <genivere.py@gmail.com>
2019-07-31 15:33:52 -0700

    Add 2 file functions .

--- student_tracker.py
+++ student_tracker.py
@@ -1,3 +1,5 @@
+import os
+
 '''Web App to keep track of student progress.'''
 
 students = []
@@ -36,8 +38,41 @@ def get_student():
             print("Invalid entry")
     
     student = {"name": name.title(), "student_id": student_id.title()}
+    students.append(student)
 
     return student 
 
-student = get_student()
-print(student)
+
+def append_file(student):
+    '''Add each student's name to a file.
+
+        Args:
+            student:    The student's name only
+    '''
+
+    try:
+        with open('students.txt', 'a') as f:
+            f.write(student['name'] + '\n')
+    except FileNotFoundError as e:
+        print(f'Error: {e}\nCould not open file.') 
+
+
+def print_file_contents():
+    '''Print the current student database.'''
+
+    with open('students.txt', 'r') as f:
+        for name in f.readlines():
+            print(name)
+
+
+enter_student = 'y'
+
+while enter_student == 'y':
+    student = get_student()
+    append_file(student)
+    enter_student = input('Enter another student? "y" for yes, and "n" for no:  ').lower()
+
+
+print('Current student list:')
+
+print_file_contents()
